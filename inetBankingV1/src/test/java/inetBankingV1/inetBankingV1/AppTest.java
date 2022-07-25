7a5684f76d26cc801cd1d5daefceb5c45a0f8e95
apmosys <apmosys@APMOSYSLT0170>
2022-07-25 11:41:33 +0530

    this is done

---
+++ inetBankingV1/src/test/java/inetBankingV1/inetBankingV1/AppTest.java
@@ -0,0 +1,12 @@
+package inetBankingV1.inetBankingV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
